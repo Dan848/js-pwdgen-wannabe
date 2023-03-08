a9933064d9c753f00166211e813b0cb842f5d4c4
Dan848 <daniele.ceravolo15@gmail.com>
2023-03-08 15:52:14 +0100

    Fixed js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-const nomeUtente = prompt ("Qual è il tuo Nome?")
-const cognomeUtente = prompt ("Qual è il tuo Cognome?")
-const coloreUtente = prompt ("Qual è il tuo Colore preferito?")
+/*Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21*/
 
-document.getElementById ("perfectPassword").innerHTML = `${nomeUtente}${cognomeUtente}${coloreUtente}21`
+const nomeUtente = prompt("Qual è il tuo Nome?");
+const cognomeUtente = prompt("Qual è il tuo Cognome?");
+const coloreUtente = prompt("Qual è il tuo Colore preferito?");
+
+document.getElementById ("perfectPassword").innerHTML = `${nomeUtente}${cognomeUtente}${coloreUtente}21`;
